cb077330a08d1e63c74c9bb0063cd454fd484863
Alex Romero <alexromeroreyes09@gmail.com>
2022-01-13 11:14:10 -0800

    Working on products handler

--- server/ProductOverview/server.js
+++ server/ProductOverview/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const { TOKEN } = require('./config.js');
 const port = 4000;
+const db = require('./database/index.js');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+
+app.get('/products', (req, res) => {
   res.send('Hello, World!');
 });
 
